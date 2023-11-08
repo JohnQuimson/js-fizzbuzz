d073b8dfb95ec2266a91b3fac7fe4bb5067d6281
JohnQuimson <john.quimson777@gmail.com>
2023-11-08 16:18:38 +0100

    fix: else

--- js/main.js
+++ js/main.js
@@ -2,26 +2,14 @@
 
 for (let i = 1; i < 101; i++) {
   console.log(i);
+
   if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
   } else if (i % 3 === 0) {
     console.log('Fizz');
   } else if (i % 5 === 0) {
     console.log('Buzz');
+  } else {
+    console.log(i);
   }
 }
-
-// //verifica multiplo 3
-//   if (i % 3 == 0) {
-//     i = multiplo3;
-
-//     //verifica multiplo 5
-//   }
-//   if (i % 5 == 0) {
-//     i = multiplo5;
-
-//     //verifica multiplo 3 e 5
-//   }
-//   if (i % 3 == 0 && i % 5 == 0) {
-//     i = multiplo35;
-//   }
