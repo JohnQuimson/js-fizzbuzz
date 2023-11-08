f439e962c7f2244b7e214ad829b949b198665066
JohnQuimson <john.quimson777@gmail.com>
2023-11-08 16:05:46 +0100

    fix: FizzBuzz

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
 'use strict';
 
-//DICHIARAZIONI
-const multiplo3 = 'Fizz';
-const multiplo5 = 'Buzz';
-const multiplo35 = 'FizzBuzz';
-
 for (let i = 1; i < 101; i++) {
   console.log(i);
-
-  if (i % 3 == 0) {
-    i = multiplo3;
-    console.log(i);
-  } else if (i % 5 == 0) {
-    i = multiplo5;
-  } else if (i % 3 == 0 && i % 5 == 0) {
-    i = multiplo35;
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
   }
 }
 
