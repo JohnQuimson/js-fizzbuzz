760e6f0e39a37772e7e912d63fc53455546271f9
JohnQuimson <john.quimson777@gmail.com>
2023-11-08 17:14:52 +0100

    feat: sostituzione stringhe

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@
 const elementUl = document.querySelector('ul.lista');
 
 for (let i = 1; i < 101; i++) {
-  const elementLi = `<li class = "box">${i}</li>`;
-  elementUl.innerHTML += elementLi;
+  //Aggiungo elementi li in elementUl
+  let elementLi = `<li class = "box">${i}</li>`;
+
+  //Condizioni
   if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
+    elementLi = `<li class="box">FizzBuzz</li>`;
   } else if (i % 3 === 0) {
     console.log('Fizz');
+    elementLi = `<li class="box">Fizz</li>`;
   } else if (i % 5 === 0) {
     console.log('Buzz');
-  } else {
-    console.log(i);
+    elementLi = `<li class="box">Buzz</li>`;
   }
+  elementUl.innerHTML += elementLi;
 }
