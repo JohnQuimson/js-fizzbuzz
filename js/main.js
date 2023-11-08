bcbdb2c73f77c01dd8b007cd793da7f4ac426274
JohnQuimson <john.quimson777@gmail.com>
2023-11-08 16:56:45 +0100

    feat: style web

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
-for (let i = 1; i < 101; i++) {
-  console.log(i);
+const elementUl = document.querySelector('ul.lista');
 
+for (let i = 1; i < 101; i++) {
+  const elementLi = `<li class = "box">${i}</li>`;
+  elementUl.innerHTML += elementLi;
   if (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
   } else if (i % 3 === 0) {
